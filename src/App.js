5ad0a30193438b28844e9c9df0a6f79bc2350bc7
Sankalp-G <sankalp.gawali@gmail.com>
2023-03-27 20:53:23 +0530

    add threejs stage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Canvas } from '@react-three/fiber'
+import { Stage, OrbitControls, Environment } from '@react-three/drei'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+
 function App() {
   return (
-    <div className="App">
+    <Canvas gl={{ logarithmicDepthBuffer: true }} shadows camera={{ position: [-15, 0, 10], fov: 25 }}>
+      <fog attach="fog" args={['black', 15, 21.5]} />
 
-    </div>
+      <Stage intensity={0.5} environment="city" shadows={{ type: 'accumulative', bias: -0.001 }} adjustCamera={false}></Stage>
+
+      <OrbitControls autoRotate autoRotateSpeed={0.05} enableZoom={false} makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+      <EffectComposer disableNormalPass>
+        <Bloom luminanceThreshold={1} mipmapBlur />
+      </EffectComposer>
+      <Environment background preset="sunset" blur={0.8} />
+    </Canvas>
   );
 }
 
