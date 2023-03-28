b2bdf2601a95d86267929bf57da827e852bb53e0
Sankalp-G <sankalp.gawali@gmail.com>
2023-03-28 20:50:45 +0530

    add bulb mesh and materials

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { Canvas } from '@react-three/fiber'
 import { Stage, OrbitControls, Environment } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import Bulb from './Bulb'
 
-function App() {
+function Model() {
   return (
     <Canvas gl={{ logarithmicDepthBuffer: true }} shadows camera={{ position: [-15, 0, 10], fov: 25 }}>
       <fog attach="fog" args={['black', 15, 21.5]} />
 
-      <Stage intensity={0.5} environment="city" shadows={{ type: 'accumulative', bias: -0.001 }} adjustCamera={false}></Stage>
+      <Stage intensity={0.5} environment="city" shadows={{ type: 'accumulative', bias: -0.001 }} adjustCamera={false}>
+        <Bulb />
+      </Stage>
 
-      <OrbitControls autoRotate autoRotateSpeed={0.05} enableZoom={false} makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+      <OrbitControls autoRotate autoRotateSpeed={0.2} maxZoom={2} minZoom={1} />
       <EffectComposer disableNormalPass>
-        <Bloom luminanceThreshold={1} mipmapBlur />
+        <Bloom luminanceThreshold={1} intensity={20} radius={1} mipmapBlur />
       </EffectComposer>
       <Environment background preset="sunset" blur={0.8} />
     </Canvas>
   );
 }
 
-export default App;
+export default Model;
